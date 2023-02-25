3ac98bc7dfaecb58b98b86a3b57cf86e5b7b3f89
Gastón Alvarez <gaston_a96@hotmail.com>
2023-02-25 16:33:46 -0300

    first commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,6 @@
+
+export default function Button({ styles }) {
+  return (
+    <button type="button" className={`${styles} py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none rounded-[10px]`}>Get Started</button>
+  )
+}
---
+++ src/components/Clients.jsx
@@ -0,0 +1,18 @@
+import { clients } from "../constants"
+import styles from "../style"
+
+export default function Clients() {
+  return (
+    <section className={`${styles.flexCenter} my-4`}>
+      <div className={`${styles.flexCenter} flex-wrap w-full`}>
+        {
+          clients.map((client) => (
+            <div key={client.id} className={`${styles.flexCenter} flex-1 sm:min-w-[192px] min-w-[120px] mx-2 my-2`}>
+              <img src={client.logo} alt="client" className="sm:w-[192px] w-[100px] object-contain"/>
+            </div>
+          ))
+        }
+      </div>
+    </section>
+  )
+}
